df44c4e9add50be627a9017c89b8bcab0ea31ae2
Will Evans <william@huggg.me>
2022-12-01 14:56:41 +0000

    Initial Commit

---
+++ 1.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+)
+
+type Food struct {
+	calories int64
+}
+
+type Elf struct {
+	foods        []Food
+	currCalories int64
+}
+
+func (elf Elf) sumFoodCalories() int64 {
+	var calories int64
+
+	calories = 0
+
+	for _, food := range elf.foods {
+		calories += food.calories
+	}
+
+	return calories
+}
+
+func (elf *Elf) storeFood(food Food) {
+	elf.foods = append(elf.foods, food)
+	elf.currCalories += food.calories
+}
+
+func parseInputFile(path string) []Elf {
+	f, err := os.Open(path)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	elves := []Elf{{foods: []Food{}, currCalories: 0}}
+	currentElfIndex := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if line == "" {
+			elves = append(elves, Elf{foods: []Food{}, currCalories: 0})
+			currentElfIndex += 1
+			continue
+		}
+
+		calories, err := strconv.ParseInt(line, 10, 64)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		food := Food{calories: calories}
+
+		elves[currentElfIndex].storeFood(food)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return elves
+}
+
+func findElfCarryingTheMostFood(elves []Elf) Elf {
+	var currentMaxElf Elf
+	var currentMaxCalories int64
+
+	currentMaxElf = elves[0]
+	currentMaxCalories = 0
+
+	for _, elf := range elves {
+		calories := elf.currCalories
+
+		if elf.currCalories > currentMaxCalories {
+			currentMaxElf = elf
+			currentMaxCalories = calories
+		}
+	}
+
+	return currentMaxElf
+}
+
+func sortElvesByCalories(elves []Elf) []Elf {
+	sort.Slice(elves, func(i, j int) bool {
+		return elves[i].currCalories > elves[j].currCalories
+	})
+
+	return elves
+}
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	elves := parseInputFile("./1-input.txt")
+
+	fmt.Println("Num elves: ", len(elves))
+
+	largestElf := findElfCarryingTheMostFood(elves)
+
+	fmt.Println("Largest elf: ", largestElf.currCalories)
+
+	elves = sortElvesByCalories(elves)
+
+	fmt.Println("Largest 3 elves: ", elves[0].currCalories, elves[1].currCalories, elves[2].currCalories)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/will3942/advent-of-code-2022
+
+go 1.18
